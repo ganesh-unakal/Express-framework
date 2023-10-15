59837afd7d7ecc0b4262523a7c02023c1b01eb47
Ganesh Unakal <122563639+ganesh-unakal@users.noreply.github.com>
2023-10-15 11:59:33 +0530

    Serving HTML , CSS and JS (VIEWS)

--- app.js
+++ app.js
@@ -2,18 +2,25 @@ const express = require('express');
 const bodyParser = require('body-parser')
 
 const app = express()
+const port = 3000
+const path = require('path')
 
 const adminroutes = require('./Routes/admin')
 const shoproutes = require('./Routes/shop')
+const contactRoutes = require('./Routes/contactus')
 
 app.use(bodyParser.urlencoded({extended: false}))
 
 app.use('/admin',adminroutes)
 app.use(shoproutes)
+app.use(contactRoutes)
+app.use(express.static(path.join(__dirname,'public')))
 
 app.use((req,res,next) => {
-      res.status(404).send('<h1>Page not found</h1>')   
+      res.status(404).sendFile(path.join(__dirname,'views','404.html'))   
 })
 
 
-app.listen(3000);
+app.listen(port, () => {
+      console.log(`server is running on port ${port}`)
+});
